b2657223ee522f7b24eb62309386772471d74fe3
Nisu Joshi <nj95+olea@protonmail.com>
2024-07-25 05:03:36 -0400

    initial flake for `nix develop`

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "OleaCMS monorepo with backend and frontend";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells = {
+        default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.emacs
+            pkgs.stdenv.cc.cc.lib
+            pkgs.nodejs_20
+            pkgs.nodePackages.typescript
+            pkgs.nodePackages.typescript-language-server
+            pkgs.bun
+            # pkgs.pnpm
+            pkgs.tree
+            pkgs.sqlite
+            pkgs.starship
+          ];
+          shellHook = ''
+            echo ""
+            echo "Welcome to the OleaCMS backend development environment"
+            eval "$(starship init bash)"
+            export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib";
+          '';
+        };
+      };
+    });
+}
