8d04f075da69bcf2a61df05413f637a8f16de856
nisu <nisarg.joshi.95@gmail.com>
2024-08-01 08:57:29 -0400

    feat(V1_FE-1): hide navbar center on login page, setup routing

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,6 +2,7 @@ import "./style.css";
 import "./tracing";
 import PineconeRouter from "pinecone-router";
 import Alpine from "alpinejs";
+import component from "alpinejs-component";
 import morph from "@alpinejs/morph";
 import { themeChange } from "theme-change";
 
@@ -13,6 +14,8 @@ import LoginPage from "./pages/login/login";
 (window as any).Alpine = Alpine;
 Alpine.plugin(morph);
 Alpine.plugin(PineconeRouter);
+Alpine.plugin(component);
+(window as any).PineconeRouter.settings.templateTargetId = "app";
 (window as any).htmx = htmx;
 (window as any).htmx.config.selfRequestsOnly = false;
 
